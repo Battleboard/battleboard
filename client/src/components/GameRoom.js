f9e31b6eeef480ad8a386273591be55305dd1104
therjmurray <therjmurray@gmail.com>
2021-04-02 01:38:18 -0400

    basic setup for Gameroom with area preped for battle info

--- client/src/components/GameRoom.js
+++ client/src/components/GameRoom.js
@@ -34,22 +34,24 @@ const GameRoom = ({setGameRoom}) => {
         }
     }
 
-	return  <div>
-        <h4>Player 1 Health: {store.getState().user.gameRoom[0] && store.getState().user.gameRoom[0].health}</h4>
-        <h4>Player 2 Health: {store.getState().user.gameRoom[1] && store.getState().user.gameRoom[1].health}</h4>
-        
-        <div style={{display: 'flex'}}>
+	return <div style={{display: 'flex', height: '100%', flexDirection: 'column'}}>
+        <div style={{width: '100%', flexGrow: 1, background: 'green'}}>
+            {/* Area to display battle info */}
+        </div>
+        <div style={{display: 'flex', flexGrow: 2}}>
             {clients.map((client, index) => {
-                return <div key={index} style={{border: '3px solid #333', display: 'flex', margin: 10}}>    
-                    {client.spells.map((spell, index) => {
-                        return <div key={index}>
-                            <Card  spell={spell} action={() => spellClicked(spell, client.clientId)}/> {spell.name} - {client.clientId}
-                        </div>
-                    })}
+                return <div key={index} style={{border: '3px solid #333', display: 'flex', margin: 0, width: '50%', flexDirection: 'column'}}>
+                    <h4 style={{textAlign: 'center', width: '100%'}}>Player {index + 1} Health: {store.getState().user.gameRoom[index] && store.getState().user.gameRoom[index].health}</h4> 
+                    <div style={{width: '60%', display: 'flex', flexWrap: 'wrap', height: 550, margin: '10px auto', justifyContent: 'center'}}>
+                        {client.spells.map((spell, index) => {
+                            return <div key={index}>
+                                <Card  spell={spell} action={() => spellClicked(spell, client.clientId)}/>
+                            </div>
+                        })}
+                    </div>
                 </div>
             })}
-        </div>
-
+        </div>       
     </div>
 };
 
@@ -57,10 +59,4 @@ const mapStateToProps = state => ({
     auth: state.auth
 });
 
-
-
-
-//export default CreateGame;
 export default connect(mapStateToProps, {setGameRoom})(GameRoom);
-
-//export default GameRoom;
