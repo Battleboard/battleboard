441a9ec12fa1a60433caceaeb27ca43cda08ba0b
therjmurray <therjmurray@gmail.com>
2021-04-03 16:30:36 -0400

    move AuthLinks code into Landing

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -33,9 +33,39 @@ const GuestLinks = () => {
 }
 
 const Landing = () => {
-  const auth = useSelector(state => state.auth)
+	const dispatch = useDispatch()
+	const phase = useSelector(state => state.user.phase)
+	const connection = useSelector(state => state.user.connection)
+	const clients = useSelector(state => state.user.gameRoom);
+	const auth = useSelector(state => state.auth)
+	
+	const setContent = () => {
+		switch(phase){
+			case 'select-spells':
+				return <>
+					<Spells />
+					<SelectedSpells />
+				</>
+			case 'gameroom':
+				return <CreateGame />
+			case 'battle':
+				return <GameRoom connection={connection} />
+			case 'battle-over':
+				return <div style={{display: 'flex', flexDirection: 'column'}}>
+					<p>{`Player 1 hp: ${clients[0].health}`}</p>
+					<p>{`Player 2 hp: ${clients[1].health}`}</p>
+					{clients[0].health > clients[1].health ? <p>player 1 wins</p> : <p>player 2 wins</p>}
+					<button onClick={() => {
+						dispatch(setPhase('select-spells'))
+						dispatch(resetGame())
+					}}>restart</button>
+				</div>
+			default:
+				return 'AuthLinks Switch Broken (Error 42069)'
+		}
+	}
 
-	return auth && auth.isAuthenticated ? <AuthLinks /> : <GuestLinks/>
+	return auth && auth.isAuthenticated ? setContent() : <GuestLinks/>
 
 }
 
