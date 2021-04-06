d155bcce3bd96b7a8224b80641aee4bf260d3a37
therjmurray <therjmurray@gmail.com>
2021-04-06 01:26:39 -0400

    update profile

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,18 +1,25 @@
-import { useSelector, useDispatch } from 'react-redux'
-import { setPhase } from '../actions/userActions'
-import Button from './styled/Button'
+import { useSelector } from 'react-redux'
+
+const stat_container_styles = {display: 'flex', flexDirection: 'column'}
 
 const Profile = () => {
-    const dispatch = useDispatch()
-    const username = useSelector(state => state.auth.name)
     const user = useSelector(state => state.user)
 
     return <div>
-    <Button onClick={() => dispatch(setPhase('gameroom'))}>Back</Button>
-        {username}
-        Wins: {user.wins}
-        Losses: {user.losses}
-        Draws: {user.draws}
+        <div style={{display: 'flex', width: 500, margin: '10px auto', justifyContent: 'space-around', textAlign: 'center', fontSize: 20, fontWeight: 'bold'}}>
+            <div style={stat_container_styles}>
+                <span>Wins</span>
+                <span>{user.wins}</span>
+            </div>
+            <div style={stat_container_styles}>
+                <span>Losses</span>
+                <span>{user.losses}</span>
+            </div>
+            <div style={stat_container_styles}>
+                <span>Draws</span>
+                <span>{user.draws}</span>
+            </div>
+        </div>
     </div>
 }
 
