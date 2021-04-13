8fc6910ac2a90f572a26240cc92e24ef1b1f89b8
Brent3ds <bhergott1@gmail.com>
2021-04-13 00:12:41 -0400

    new component

--- client/src/components/CardInfoToImage.js
+++ client/src/components/CardInfoToImage.js
@@ -3,8 +3,17 @@ import Spells from "./Spells"
 
 {Object.keys(spell).filter(e => e !== 'name' && e !== "source" && e !== 'debuffs' && e !== 'spellList' && e !== 'index').map((key, index) => {
 
+//mapping through an array of objects and repalcing the called apon key with a image
 
+// use if statement (case statement)
 
-const infoImage = {
-    if (spell.filter)
+const infoImage = () => {
+    switch(key) {
+        case "damage":
+            return <img src ="/images/icons/sword.svg" style={{width: 15, height: 15}} />
+        case "heal":
+            return <img src ="/images/icons/sword.svg" style={{width: 15, height: 15}} />
+        case "shield":
+            return <img src ="/images/icons/sword.svg" style={{width: 15, height: 15}} />
+    }
 }
